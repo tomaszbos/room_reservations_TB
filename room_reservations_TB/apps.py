9e7f9e65bea3532cab4e7f9257003b4b9aec22f8
Tomasz Bos <tomaszbos@gmail.com>
2020-07-18 10:45:45 +0200

    Initial commit 2.0

---
+++ room_reservations_TB/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class RoomReservationsTbConfig(AppConfig):
+    name = 'room_reservations_TB'
---
+++ settings/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for room_reservations_TB project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'room_reservations_TB.settings')
+
+application = get_wsgi_application()
